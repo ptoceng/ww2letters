3a217a92e276fb811a0fbad2e49c33e04ea3f491
Patrick Oconnor <ptoceng@gmail.com>
2023-10-24 15:26:42 +0100

    image carsoule styling

--- src/Components/Letter/Letter.js
+++ src/Components/Letter/Letter.js
@@ -1,5 +1,6 @@
 import ImageSlider from "../../Components/ImageSlider/ImageSlider";
 import { Link } from "react-router-dom";
+import "./Letter.scss";
 
 function Letter({ letter }) {
     window.scrollTo(0, 0)
@@ -8,7 +9,7 @@ function Letter({ letter }) {
       <div className="letters__card">
         <h3 className="letters__card-title"></h3>
         <h4 className="letters__card-date">{letter.date}</h4>
-        <ImageSlider images={letter.images} envelope={letter.envelope} />
+        <ImageSlider className="imageslider" images={letter.images} envelope={letter.envelope} />
         <p className="letters__card-text">{letter.text}</p>
       </div>
     </>
