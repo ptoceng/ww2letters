43f759a3f59e440f16e85bc5e1bbce2a57dd8077
Patrick Oconnor <ptoceng@gmail.com>
2023-10-25 16:20:43 +0100

    desktop responsive

--- src/Components/Letter/Letter.js
+++ src/Components/Letter/Letter.js
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 import "./Letter.scss";
 
 function Letter({ letter }) {
-    window.scrollTo(0, 0)
+  window.scrollTo(0, 0);
   return (
     <>
       <div className="letters__card">
-        <h3 className="letters__card-title"></h3>
         <h4 className="letters__card-date">{letter.date}</h4>
-        <ImageSlider className="imageslider" images={letter.images} envelope={letter.envelope} />
-        <p className="letters__card-text" dangerouslySetInnerHTML={{__html: letter.text}}></p>
+        <div className="letters__card--imagetextdiv">
+          <ImageSlider
+            className="imageslider"
+            images={letter.images}
+            envelope={letter.envelope}
+          />
+          <p
+            className="letters__card-text"
+            dangerouslySetInnerHTML={{ __html: letter.text }}
+          ></p>
+        </div>
       </div>
     </>
   );
