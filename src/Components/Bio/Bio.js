6cae9f29e543fffb55bad39042544b0475597c70
Patrick Oconnor <ptoceng@gmail.com>
2023-11-27 15:17:39 +0000

    bio spelling

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -14,10 +14,10 @@ function Bio() {
           </div>
           <div className="bio__textbutton-div">
             <p className="bio__text">
-              Sergeant Thomas Patrick O’Connor server with the 44th Division
-              during World War Two from 1943 - 1945. This project aims to tell
+              Sergeant Thomas Patrick O’Connor served with the 44th Division
+              during World War II from 1943 - 1945. This project aims to tell
               the story of his service though the letters he sent home to his
-              girlfriend (and later wife) Cathliene O’Connor. The letters span
+              girlfriend (and later wife) Cathleen O’Connor. The letters span
               the years 1943 - 1947 and cover his time in Ft. Lewis (where he
               trained), France, Germany, Austria, and after his return.
             </p>
