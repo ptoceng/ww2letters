89b571a49a3da191a523397a6674c7e2345e0631
Patrick Oconnor <ptoceng@gmail.com>
2023-12-01 11:54:47 +0000

    fixes

--- src/Components/MoreArtifacts/MoreArtifacts.js
+++ src/Components/MoreArtifacts/MoreArtifacts.js
@@ -13,25 +13,33 @@ function MoreArtifacts() {
         <tbody>
           <tr>
             <td>
+              <a href="/images/dischargecert.webp" target="_blank">
               <img  src="/images/dischargecert.webp" />
+              </a>
             </td>
             <td>Thomas O'Connors Discharge Certificate</td>
           </tr>
           <tr>
             <td>
+              <a href="/images/dischargepaper.webp" target="_blank">
               <img  src="/images/dischargepaper.webp" />
+              </a>
             </td>
             <td>Thomas O'Connors Discharge Papers</td>
           </tr>
           <tr>
             <td>
+              <a href="/images/grandmagrandpa.webp" target="_blank">
               <img  src="/images/grandmagrandpa.webp" />
+              </a>
             </td>
             <td>Cathleen and Tom on their Wedding Day</td>
           </tr>
           <tr>
             <td>
+              <a href="/images/queenelizabetharticle.pdf" target="_blank">
               <img  src="/images/returnship1.webp" />
+              </a>
             </td>
             <td>New York Times Article Detailing the Return on the Queen Mary</td>
           </tr>
