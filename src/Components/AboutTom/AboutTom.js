8c68c7b623245c17de03f77aed26784846513922
Patrick Oconnor <ptoceng@gmail.com>
2023-12-03 12:40:00 +0000

    aboutpagefix

--- src/Components/AboutTom/AboutTom.js
+++ src/Components/AboutTom/AboutTom.js
@@ -3,20 +3,22 @@ import "./AboutTom.scss";
 function AboutTom() {
   return (
     <>
-      <section className="abouttom">
+      <article className="abouttom">
         <h2 className="abouttom__title">About SGT. Thomas Patrick O'Connor</h2>
-        <div className="abouttom__section-one">
-          <div className="abouttom__image-div">
-            <img
-              className="abouttom__image-friends"
-              src="/images/tomwithfriend.webp"
-            ></img>
-            <p className="abouttom__image-text">
+        <div className="sectionone">
+          <div className="sectionone__imagediv">
+            <a href="/images/tomwithfriend.webp" target="_blank">
+              <img
+                className="sectionone__image"
+                src="/images/tomwithfriend.webp"
+              ></img>
+            </a>
+            <p className="sectionone__image-txt">
               SGT. O'Connor, pictured left with a friend. aprox 1944
             </p>
           </div>
-          <div>
-            <p className="abouttom__text text1">
+          <div className="sectionone__txt-div">
+            <p className="sectionone__txt">
               The letters contained on this site letters were written by Thomas
               P. O’Connor during his time in the Army during World War II. The
               letters were saved by Catherine O’Connor and kept in a dark
@@ -30,88 +32,100 @@ function AboutTom() {
               letters. The letters span the period from 7/5/43 to 8/30/45.
               During the period when the letters were written, Dad / Grandpa was
               between 25 and 27 and Mom / Grandma was between 23 and 25 years
-              old.{" "}
+              old.
             </p>
           </div>
         </div>
-        <div className="abouttom__section-two">
-          <div className="abouttom__image-div">
-            <img
-              className="abouttom__image-closedcase"
-              src="/images/caseclosed.webp"
-            ></img>
-            <p className="abouttom__image-text">
-              {" "}
-              The letters as they were stored for 70 years
+        <div className="sectiontwo">
+          <div className="sectiontwo__imagesdiv">
+            <div className="sectiontwo__imagediv">
+              <a href="/images/caseclosed.webp" target="_blank">
+                <img
+                  className="sectiontwo__image"
+                  src="/images/caseclosed.webp"
+                ></img>
+              </a>
+              <p className="sectiontwo__image-txt">
+                The letters as they were stored for 70 years
+              </p>
+            </div>
+            <div className="sectiontwo__imagediv">
+              <a href="/images/caseopen.webp" target="_blank">
+                <img
+                  className="sectiontwo__image"
+                  src="/images/caseopen.webp"
+                ></img>
+              </a>
+              <p className="sectiontwo__image-txt">Inside the case</p>
+            </div>
+          </div>
+          <div className="sectiontwo__txt-div">
+            <p className="sectiontwo__txt">
+              I obtained a copy of his <a href="/images/dischargepaper.webp" target="_blank">Military Personnel File </a> and tracked the
+              movement of his division using on-line web sites. He joined the
+              army on June 16, 1941, and served until October 16, 1945. He was a
+              member of the 44th Infantry Division, 71st Regiment which was
+              initially comprised of men from the New York and New Jersey area.
+              The Division was trained in Fort Lewis, Washington and then went
+              to Louisiana and Kansas for predeployment training.
+              <br></br>
+              <br></br>The 44th division departed the United States via Boston
+              and arrived by ship in France on September 15, 1944. The 44th
+              Division marched across the width of France and entered Germany in
+              late March of 1945. Dad / Grandpa was evacuated to Metz, France in
+              April of 1945 to receive treatment for pneumonia and then returned
+              to Germany. The Regiment was in Germany for approximately 33 days
+              and then entered Austria for 4 days. The Division encountered
+              enemy troops and was involved in numerous battles with German
+              forces throughout this period. The total repotted battle
+              casualties of the 44th Division was 5,655 which included 1,038 men
+              killed in action.
+            </p>
+            <p className="sectiontwo__txt-dsk">
+              The Division returned to France and then the United States on the
+              Queen Elizabeth cruise ship which entered New York harbor on July
+              21st, 1945 with over 14,000 men. He was discharged from the Army
+              on October 16, 1945, with the rank of Sergeant (Technician 4th
+              Grade, discharge certificate is below). The letters dated from
+              September 15, 1944 to May 4, 1945 were written while the army was
+              in the field and were censored prior to delivery. The dates given
+              in the text of the letters are different than the postmark dates
+              on the envelopes. Dad / Grandpa almost never talked about the war
+              or his service. He said he was in France, that he was involved in
+              the Battle of the Bulge, and he came back to the United States on
+              the Queen Elizabeth but besides that we knew almost nothing about
+              his service.
             </p>
           </div>
-          <div className="abouttom__image-div">
-            <img
-              className="abouttom__image-opencase"
-              src="/images/caseopen.webp"
-            ></img>
-            <p className="abouttom__image-text"> Inside the case</p>
+        </div>
+        <div className="sectionthree">
+          <div className="sectionthree__imagediv">
+            <a href="/images/map.webp" target="_blank">
+              <img src="/images/map.webp"></img>
+            </a>
+            <p className="sectionthree__image-txt">
+              44th Infantry Division movements across Europe
+            </p>
+          </div>
+          <div className="sectionthree__txt-div">
+            <p className="sectionthree__txt">
+            The Division returned to France and then the United States on the
+              Queen Elizabeth cruise ship which entered New York harbor on July
+              21st, 1945 with over 14,000 men. He was discharged from the Army
+              on October 16, 1945, with the rank of Sergeant  Technician 4th
+              Grade. The letters dated from
+              September 15, 1944 to May 4, 1945 were written while the army was
+              in the field and were censored prior to delivery. The dates given
+              in the text of the letters are different than the postmark dates
+              on the envelopes. Dad / Grandpa almost never talked about the war
+              or his service. He said he was in France, that he was involved in
+              the Battle of the Bulge, and he came back to the United States on
+              the Queen Elizabeth but besides that we knew almost nothing about
+              his service.
+            </p>
           </div>
         </div>
-
-        <p className="abouttom__text">
-          I obtained a copy of his Military Personnel File (copy is below) and
-          tracked the movement of his division using on-line web sites. He
-          joined the army on June 16, 1941, and served until October 16, 1945.
-          He was a member of the 44th Infantry Division, 71st Regiment which was
-          initially comprised of men from the New York and New Jersey area. The
-          Division was trained in Fort Lewis, Washington and then went to
-          Louisiana and Kansas for predeployment training.{" "}
-        </p>
-
-        <p className="abouttom__text">
-          The 44th division departed the United States via Boston and arrived by ship in
-          France on September 15, 1944. The 44th Division marched across the
-          width of France and entered Germany in late March of 1945. Dad /
-          Grandpa was evacuated to Metz, France in April of 1945 to receive
-          treatment for pneumonia and then returned to Germany. The Regiment was
-          in Germany for approximately 33 days and then entered Austria for 4
-          days. The Division encountered enemy troops and was involved in
-          numerous battles with German forces throughout this period. The total
-          repotted battle casualties of the 44th Division was 5,655 which
-          included 1,038 men killed in action.{" "}
-        </p>
-        <div className="abouttom__image-div">
-          <img className="abouttom__image" src="./images/map.webp"></img>
-          <p className="abouttom__image-text">
-            {" "}
-            44th Infantry Division movements across Europe
-          </p>
-        </div>
-        <p className="abouttom__text">
-          The Division returned to France and then the United States on the
-          Queen Elizabeth cruise ship which entered New York harbor on July
-          21st, 1945 with over 14,000 men. He was discharged from the Army on
-          October 16, 1945, with the rank of Sergeant (Technician 4th Grade,
-          discharge certificate is below). The letters dated from September 15,
-          1944 to May 4, 1945 were written while the army was in the field and
-          were censored prior to delivery. The dates given in the text of the
-          letters are different than the postmark dates on the envelopes. Dad /
-          Grandpa almost never talked about the war or his service. He said he
-          was in France, that he was involved in the Battle of the Bulge, and he
-          came back to the United States on the Queen Elizabeth but besides that
-          we knew almost nothing about his service.
-        </p>
-        <div className="abouttom__image-div">
-          <img className="abouttom__image" src="./images/returnship1.webp"></img>
-          <p className="abouttom__image-text">
-            {" "}
-            The 44th division returned to great fanfare abord the Queen Mary
-          </p>
-        </div>
-        <div className="abouttom__image-div">
-          <img className="abouttom__image" src="./images/returnship2.webp"></img>
-          <p className="abouttom__image-text">
-            {" "}
-            The men of the 44th after returning to New York
-          </p>
-        </div>
-      </section>
+      </article>
     </>
   );
 }
